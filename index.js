d9cb9af947565f3a4d5b27c606d5de14a915feb3
Volkan <14837778+volcain-io@users.noreply.github.com>
2023-08-02 12:07:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 function myfunction(){
+    const x = 1024; //min value
+    const y = 9999; // max value
+    const deg = Math.floor(Math.random() * (x - y)) + y;
 
+    document.getElementById('box').style.transform = "rotate("+deg+"deg)";
 
-	var x = 1024; //min value
-	var y = 9999; // max value
-
-	var deg = Math.floor(Math.random() * (x - y)) + y;
-
-	document.getElementById('box').style.transform = "rotate("+deg+"deg)";
-
-	var element = document.getElementById('mainbox');
-	element.classList.remove('animate');
-	setTimeout(function(){
-		element.classList.add('animate');
-	}, 5000); //5000 = 5 second
+    const element = document.getElementById('mainbox');
+    element.classList.remove('animate');
+    setTimeout(function(){
+        element.classList.add('animate');
+    }, 5000);
 }
